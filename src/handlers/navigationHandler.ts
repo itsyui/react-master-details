100fac9b76a3f1f0aa45f2e70ee39ab3f46f4c2f
Riyaj Ahamed <riyaj.ahamed@pogeyan.com>
2021-06-23 14:38:29 +0530

    React master details sample application with ItsyUI components

---
+++ src/handlers/navigationHandler.ts
@@ -0,0 +1,36 @@
+import { ICustomStateMachineData, ICustomStateMachineProvider, WidgetsFactory, DataLoaderFactory } from "@itsy-ui/core";
+
+const dataLoader = WidgetsFactory.instance.services["DataLoaderFactory"] as DataLoaderFactory;
+const customStateProvider = dataLoader.getLoader<ICustomStateMachineProvider>("customStateProvider");
+
+function doMasterGridSelectedRowsDone(evt: any) {
+    return (getState: any, dispatch: any, transition: any) => {
+        const selectedRows = evt.selectedRows;
+        transition({
+            type: "NAVIGATE_URL",
+            url: `/details?id=${selectedRows[0].id}`,
+        });
+    };
+}
+
+const tabsGrid: ICustomStateMachineData = {
+    stateJSON: {
+        "states": {
+            "gridSelectedRows": {
+                "onEntry": [
+                    "onMasterGridSelectedRowsDone",
+                ],
+                "on": {
+                    "GRID_SELECTED_ROWS_DONE": "gridSelectedRowsDone",
+                },
+            },
+        },
+    },
+    mapDispatchToAction: (dispatch) => {
+        return {
+            onMasterGridSelectedRowsDone: (evt) => dispatch(doMasterGridSelectedRowsDone(evt)),
+        };
+    },
+};
+
+customStateProvider.registerCustomStateMachine("GridWidget", { "id": "master_details" }, tabsGrid);
